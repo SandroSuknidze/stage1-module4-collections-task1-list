aefda70fece19daee3f2c767783af59dfbca3e68
Sandro Ubuntu <sandro.suknidze.1@btu.edu.ge>
2024-11-01 19:56:54 +0400

    #1

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> list = new ArrayList<>();
+
+        int count = 1;
+        for (String s : sourceList) {
+            if (count % 3 == 0) {
+                list.add(s);
+                list.add(s);
+            }
+            count++;
+        }
+
+        return list;
     }
 }
